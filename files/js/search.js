1c9baf46874e32e4ef163d2bd5da6a8e9cfa13ec
Arden Chen <lightningfrog11@gmail.com>
2018-04-30 16:13:01 -0400

    fixed redirect
    Searching something now searches it on tripadvisor.

--- files/js/search.js
+++ files/js/search.js
@@ -1,6 +1,7 @@
 function searchButton() {
     var searched = document.getElementById("search").value;
-    var url = "https://www.tripadvisor.com/Search?q="+searched;
+    var url = 'http://www.tripadvisor.com/Search?q='+searched;
     console.log(url);
-    window.location.href = 'test.html';
+    window.location.href = url;
+    return false;
 }
