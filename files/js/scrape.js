0f7bf839f0c07f81a818f62ffd81e08edf421f3d
Arden Chen <lightningfrog11@gmail.com>
2018-05-01 17:31:56 -0400

    Scrape is successful
    output.json and outputRatings.json are the two files where output happens. everytime another test happens we might have to delete these files or something idk. In scrape.js, i am currently using a test restaurant's website, and with any restaurant i can scrape its site.
    
    In order to run this, you have to run the command node scrape.js in the directory, and the server is located on port 8081. if you get a message called "check your console" then happy days

--- files/js/scrape.js
+++ files/js/scrape.js
@@ -5,10 +5,10 @@ var cheerio = require('cheerio');
 var app = express();
 
 app.get('/scrape', function (req, res) {
-
-    var url = getSearchedUrl();
+    //'http://www.tripadvisor.com/Search?q=food#&ssrc=e&o=0'
+    var url = 'https://www.tripadvisor.com/Restaurant_Review-g186273-d733804-Reviews-Food_for_Friends-Brighton_East_Sussex_England.html';
     var restaurantNameList = [];
-
+    var nameList;
 
     //All the web scraping magic will happen here
     request(url, function (error, response, html) {
@@ -22,28 +22,66 @@ app.get('/scrape', function (req, res) {
 
             // Finally, we'll define the variables we're going to capture
 
-            var name, starrating, textrating;
+            var name, starrating;
             var json = {
                 name: "",
-                starrating: "",
-                rating: ""
+                starrating: ""
+            };
+
+            var ratingsJson = {
+                ratings: []
             };
             // FINISHED LOADING PAGE
-            $('title').each(function (i) {
-                restaurantNameList.push(i.getElementByTagName("span"));
+            $('.heading_title').filter(function () {
+
+                // Let's store the data we filter into a variable so we can easily see what's going on.
+
+                var data = $(this);
+
+                // In examining the DOM we notice that the title rests within the first child element of the header tag. 
+                // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+                name = data.text();
+
+                // Once we have our title, we'll store it to the our json object.
+
+                json.name = name;
+            });
+            var reviews = [];
+            $('.review-container').each(function (i) {
+                var data = $(this);
+                review = data.find('.partial_entry').text();
+                reviews.push(review);
+            });
+            ratingsJson.ratings = reviews;
+
+            $('.ui_bubble_rating bubble_45').filter(function () {
+                var data = $(this);
+                starrating = data.attr('alt');
+                json.starrating = starrating;
             });
         };
-    });
-    var nameList = "";
-    restaurantNamelist.forEach(function (i) {
-        nameList += i;
-        nameList += '\n';
-    });
-    fs.writeFile('output.json', nameList, function (err) {
+        // To write to the system we will use the built in 'fs' library.
+        // In this example we will pass 3 parameters to the writeFile function
+        // Parameter 1 :  output.json - this is what the created filename will be called
+        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3 :  callback function - a callback function to let us know the status of our function
 
-        console.log('File successfully written! - Check your project directory for the output.json file');
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
 
-    })
+            console.log('File successfully written! - Check your project directory for the output.json file');
+
+        });
+        var finalRatings = {
+            ratings: ""
+        };
+        for (let i = 0; i < ratingsJson.ratings.length; i++) {
+            finalRatings.ratings += ratingsJson.ratings[i];  
+        }
+        fs.writeFile('outputRatings.json', JSON.stringify(finalRatings));
+        res.send('Check your console!')
+    });
+
 });
 
 app.listen('8081')
